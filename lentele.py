cb80da5692023deefb61d0a3bebdbee11e265dc5
Gytis Vaicius <gyteniz@gmail.com>
2023-03-24 18:20:20 +0200

    lenteles pavyzdys

--- lentele.py
+++ lentele.py
@@ -1,4 +1,15 @@
 class Atvaizdavimas:
+    def __init__(self, lang1=1, lang2=2, lang3=3, lang4=4, lang5=5, lang6=6, lang7=7, lang8=8, lang9=9):
+        self.lang1 = lang1
+        self.lang2 = lang2
+        self.lang3 = lang3
+        self.lang4 = lang4
+        self.lang5 = lang5
+        self.lang6 = lang6
+        self.lang7 = lang7
+        self.lang8 = lang8
+        self.lang9 = lang9
+
     def groteles(self):
         lang1 = 1
         lang2 = 2
@@ -11,15 +22,15 @@ class Atvaizdavimas:
         lang9 = 9
 
         print('%s' % (9 * '.     '))
-        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
         print('.%s|%s|%s.' % (str(lang1).center(15), str(lang2).center(15), str(lang3).center(15)))
         print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
-        print('%s' % (49 * '_'))
         print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('%s' % (49 * '_'))
         print('.%s|%s|%s.' % (str(lang4).center(15), str(lang5).center(15), str(lang6).center(15)))
         print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
-        print('%s' % (49 * '_'))
         print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('%s' % (49 * '_'))
         print('.%s|%s|%s.' % (str(lang7).center(15), str(lang8).center(15), str(lang9).center(15)))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
         print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
         print('%s' % (9 * '.     '))
