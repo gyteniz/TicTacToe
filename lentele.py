69cfd41e0680eaca1dc1f9aebff404f7eec6da35
Gytis Vaicius <gyteniz@gmail.com>
2023-03-24 10:06:49 +0200

    lenteles prototipas

---
+++ lentele.py
@@ -0,0 +1,20 @@
+
+
+suma1=111
+suma2=222
+suma3=333
+
+
+print('%s' % (9 * '.     '))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('%s' % (49 * '_'))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('%s' % (49 * '_'))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
+print('%s' % (9 * '.     '))
