e6c4a573887c028349b4f3a22038f1d5e242f826
Gytis Vaicius <gyteniz@gmail.com>
2023-03-24 10:59:07 +0200

    langeliu priskirimas ir isdestymas

--- lentele.py
+++ lentele.py
@@ -1,20 +1,26 @@
 
 
-suma1=111
-suma2=222
-suma3=333
+lang1=1
+lang2=2
+lang3=3
+lang4=4
+lang5=5
+lang6=6
+lang7=7
+lang8=8
+lang9=9
 
 
 print('%s' % (9 * '.     '))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('.%s|%s|%s.' % (str(lang1).center(15), str(lang2).center(15), str(lang3).center(15)))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
 print('%s' % (49 * '_'))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('.%s|%s|%s.' % (str(lang4).center(15), str(lang5).center(15), str(lang6).center(15)))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
 print('%s' % (49 * '_'))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(suma1).center(15),str(suma2).center(15), str(suma3).center(15)))
+print('.%s|%s|%s.' % (str(lang7).center(15), str(lang8).center(15), str(lang9).center(15)))
 print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
 print('%s' % (9 * '.     '))
