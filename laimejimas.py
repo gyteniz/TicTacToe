e953b73fbca1021783bfd9b59f24446519071bee
Gytis Vaicius <gyteniz@gmail.com>
2023-03-24 17:06:20 +0200

    lenteles pavyzdys

--- laimejimas.py
+++ laimejimas.py
@@ -1,5 +1,3 @@
-
-
 class Laimejimas:
     def __init__(self, sk1=False, sk2=False, sk3=False, sk4=False, sk5=False, sk6=False, sk7=False, sk8=False,
                  sk9=False):
@@ -13,37 +11,34 @@ class Laimejimas:
         self.sk8 = sk8
         self.sk9 = sk9
 
-
-
     def gauti_rezultata(self):
         self.sk = [self.sk1, self.sk2, self.sk3, self.sk4, self.sk5, self.sk6, self.sk7, self.sk8, self.sk9]
         if self.sk1 & self.sk2 & self.sk3 == True:
-            print("riebalas")
-            # laimejimas_print()
+            print("riebalas1")
+            self.laimejimas_print()
         elif self.sk4 & self.sk5 & self.sk6 == True:
             print("riebalas2")
-            # laimejimas_print()
+            self.laimejimas_print()
         elif self.sk7 & self.sk8 & self.sk9 == True:
             print("riebalas3")
-            # laimejimas_print()
+            self.laimejimas_print()
         elif self.sk1 & self.sk4 & self.sk7 == True:
             print("riebalas4")
-            # laimejimas_print()
+            self.laimejimas_print()
         elif self.sk2 & self.sk5 & self.sk8 == True:
             print("riebalas5")
-            # laimejimas_print()
+            self.laimejimas_print()
         elif self.sk3 & self.sk6 & self.sk9 == True:
             print("riebalas6")
-            # laimejimas_print()
+            self.laimejimas_print()
         elif self.sk1 & self.sk5 & self.sk9 == True:
             print("riebalas7")
             # laimejimas_print()
         elif self.sk3 & self.sk5 & self.sk7 == True:
             print("riebalas8")
-            # laimejimas_print()
+            self.laimejimas_print()
         else:
             False
 
-    def laimejimas_print():
+    def laimejimas_print(self):
         print("LAIMEJIMAS")
-
--- lentele.py
+++ lentele.py
@@ -1,26 +1,25 @@
+class Atvaizdavimas:
+    def groteles(self):
+        lang1 = 1
+        lang2 = 2
+        lang3 = 3
+        lang4 = 4
+        lang5 = 5
+        lang6 = 6
+        lang7 = 7
+        lang8 = 8
+        lang9 = 9
 
-
-lang1=1
-lang2=2
-lang3=3
-lang4=4
-lang5=5
-lang6=6
-lang7=7
-lang8=8
-lang9=9
-
-
-print('%s' % (9 * '.     '))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(lang1).center(15), str(lang2).center(15), str(lang3).center(15)))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('%s' % (49 * '_'))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(lang4).center(15), str(lang5).center(15), str(lang6).center(15)))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('%s' % (49 * '_'))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('.%s|%s|%s.' % (str(lang7).center(15), str(lang8).center(15), str(lang9).center(15)))
-print('%s|%s|%s' % ((16 * " "), (15 * " "),(16 * " ")))
-print('%s' % (9 * '.     '))
+        print('%s' % (9 * '.     '))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('.%s|%s|%s.' % (str(lang1).center(15), str(lang2).center(15), str(lang3).center(15)))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('%s' % (49 * '_'))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('.%s|%s|%s.' % (str(lang4).center(15), str(lang5).center(15), str(lang6).center(15)))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('%s' % (49 * '_'))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('.%s|%s|%s.' % (str(lang7).center(15), str(lang8).center(15), str(lang9).center(15)))
+        print('%s|%s|%s' % ((16 * " "), (15 * " "), (16 * " ")))
+        print('%s' % (9 * '.     '))
