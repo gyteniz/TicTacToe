4ed01260f6ee49117e2eb6f31041e61efc452435
Gytis Vaicius <gyteniz@gmail.com>
2023-03-24 11:27:42 +0200

    laimejimo reiksmes

---
+++ venv/laimejimas.py
@@ -0,0 +1,21 @@
+
+sk1=False
+sk2=False
+sk3=False
+sk4=False
+sk5=False
+sk6=False
+sk7=False
+sk8=False
+sk9=False
+
+skaiciai = [sk1, sk2, sk3, sk4, sk5, sk6, sk7, sk8, sk9]
+
+laimejimas == [True, True,True,False,False,False,False,False,False]
+laimejimas == [False, False,False,True,True,True,False,False,False]
+laimejimas == [False, False,False,False,False,False,True,True,True]
+laimejimas == [True, False,False,True,False,False,True,False,False]
+laimejimas == [False, True,False,False,True,False,False,True,False]
+laimejimas == [False, False,True,False,False,True,False,False,True]
+laimejimas == [True, False,False,False,True,False,False,False,True]
+laimejimas == [False, False,True,False,True,False,True,False,False]
